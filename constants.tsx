4eb9144e47a46be77798ee450529b2f7809734de
JJFFAABBEERR <faberprueba@gmail.com>
2026-01-13 02:05:07 -0500

    Fix image path in constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -10,7 +10,7 @@ export const MOCK_COURSES: Course[] = [
     price: 49.99,
     rating: 4.9,
     students: 1250,
-    image: '.public/ferrari.png',
+    image: './public/ferrari.png',
     category: 'Infantil',
     modules: [
       {
